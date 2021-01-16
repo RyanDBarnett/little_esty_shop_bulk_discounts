dfc86f985a098c03bece55c72b0253e138b3273e
RyanDBarnett <conner2468@gmail.com>
2021-01-16 15:46:27 -0700

    Add merchant discounts index

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,7 @@
 class Discount < ApplicationRecord
   validates_presence_of :quantity_threshold,
-                        :percentage_discount
+                        :percentage_discount,
+                        :name
 
   belongs_to :merchant
 end
