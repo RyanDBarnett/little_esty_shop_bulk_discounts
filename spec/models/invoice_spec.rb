50a9eb5458fe1bfbc22f43703ad67073c613dc4f
RyanDBarnett <conner2468@gmail.com>
2021-01-18 14:18:10 -0700

    Merchant Invoice Show Page total revenue includes discounts

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -6,23 +6,35 @@ RSpec.describe Invoice, type: :model do
     it { should validate_presence_of :merchant_id }
     it { should validate_presence_of :customer_id }
   end
+
   describe "relationships" do
     it { should belong_to :merchant }
     it { should belong_to :customer }
     it { should have_many(:items).through(:invoice_items) }
     it { should have_many :transactions}
   end
-  describe "instance methods" do
-    it "total_revenue" do
-      @merchant1 = Merchant.create!(name: 'Hair Care')
-      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
-      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
-      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-      @invoice_1 = Invoice.create!(merchant_id: @merchant1.id, customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
-      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
-      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
 
-      expect(@invoice_1.total_revenue).to eq(100)
+  describe "instance methods" do
+    describe 'total_revenue' do
+      before :each do
+        @merchant_1 = Merchant.create!(name: 'Hair Care')
+        @item_1 = @merchant_1.items.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, status: 1)
+        @item_2 = @merchant_1.items.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5)
+        @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+        @invoice_1 = Invoice.create!(merchant_id: @merchant_1.id, customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+        @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 2)
+        @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 1, unit_price: 10, status: 1)
+      end
+
+      it "returns the invoice's total revenue" do
+        expect(@invoice_1.total_revenue).to eq(110)
+      end
+
+      it "returns a discounted total revenue if a discount applies" do
+        @discount_1 = @merchant_1.discounts.create!(name: '20% Off 10 Items', percentage_discount: 20.0, quantity_threshold: 10)
+
+        expect(@invoice_1.total_revenue).to eq(90)
+      end
     end
   end
 end
